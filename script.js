57519da599298f7b51fd92660bfd19304eaec8c0
grvnt <109231551+grvnt@users.noreply.github.com>
2024-02-07 06:58:27 +0100

    add 16 squares + flex in js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const divSquares = document.querySelector('#squareDivs');
+const divSquares = document.querySelector('.squareDivs');
 let singleSquare = document.createElement('div');
 
 singleSquare.style.width = "100px";
 singleSquare.style.height = "100px";
 singleSquare.style.backgroundColor = "green";
 
-divSquares.appendChild(singleSquare);
+//divSquares.appendChild(singleSquare);
 
+for (let i = 0; i < 16; i++) {
+    divSquares.appendChild(singleSquare.cloneNode(true));
+}
 
+let flexContainer = document.querySelector('div');
+flexContainer.style.display = 'flex';
 
