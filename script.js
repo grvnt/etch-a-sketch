048b651e1df6125a56a676bf97893d04f6364b32
grvnt <109231551+grvnt@users.noreply.github.com>
2024-02-15 12:52:14 +0100

    update button click to clear innerhtml

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-let gridSize;
-
-do {
-    gridSize = prompt("Enter a number less than 100");
-    gridSize = Number(gridSize);
-} while (gridSize > 100 || isNaN(gridSize) || gridSize <= 0);
 
 
-
-const containerWidth = 960;
-
-const squareSize = containerWidth / gridSize;
-
-const squareDivs = document.querySelector('.squareDivs');
-squareDivs.style.display = 'flex';
-squareDivs.style.flexWrap = 'wrap';
-squareDivs.style.width = `${containerWidth}px`;
-
-for (let i = 0; i < gridSize * gridSize; i++) {
-    let singleSquare = document.createElement('div');
-    singleSquare.classList.add('singleSquare'); //Adding class solved the problem
-    singleSquare.style.width = `${squareSize}px`;
-    singleSquare.style.height = `${squareSize}px`;
-    singleSquare.style.backgroundColor = "teal";
+document.querySelector('#start').addEventListener('click', function () {
     
-    squareDivs.appendChild(singleSquare);
-}
+    const squareDivs = document.querySelector('.squareDivs');
+    squareDivs.innerHTML = '';
+    
+    let gridSize;
+    do {
+        gridSize = prompt("Enter a number less than 100");
+        gridSize = Number(gridSize);
+    } while (gridSize > 100 || isNaN(gridSize) || gridSize <= 0);
 
-//Event listener that changes color of square on mouse hover
+    const containerWidth = 960;
+    const squareSize = containerWidth / gridSize;
 
-const squares = document.querySelectorAll('.singleSquare');
+    squareDivs.style.display = 'flex';
+    squareDivs.style.flexWrap = 'wrap';
+    squareDivs.style.width = `${containerWidth}px`;
 
-function changeColor() {
-    this.style.backgroundColor = "orange";
-}
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        let singleSquare = document.createElement('div');
+        singleSquare.classList.add('singleSquare'); 
+        singleSquare.style.width = `${squareSize}px`;
+        singleSquare.style.height = `${squareSize}px`;
+        singleSquare.style.backgroundColor = "teal";
 
-squares.forEach(square => {
-    square.addEventListener('mouseover', changeColor);
+        singleSquare.addEventListener('mouseover', function() {
+            this.style.background = "orange";
+        });
+        
+        squareDivs.appendChild(singleSquare);
+    }
+
 });
+
+
+
+
 
 
 
